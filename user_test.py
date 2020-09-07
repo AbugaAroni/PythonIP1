1827b13168f2e6b2626ae62602ca7a0f749f99e5
AbugaAroni <abugaaroni@gmail.com>
2020-09-08 01:33:55 +0300

    add user test

--- user_test.py
+++ user_test.py
@@ -1,2 +1,27 @@
 import unittest
 from user import User
+
+class TestUser(unittest.TestCase):
+
+    def setUp(self):
+        '''
+        Set up method to run before each test cases.
+        '''
+        self.new_user = User("Abuga1","password1")
+
+    def test_init(self):
+        '''
+        test_init test case to test if the object is initialized properly
+        '''
+
+        self.assertEqual(self.new_user.uname,"Abuga1")
+        self.assertEqual(self.new_user.password,"password1")
+
+    def tearDown(self):
+            '''
+            tearDown method that does clean up after each test case has run.
+            '''
+            User.user_list = []
+
+if __name__ == '__main__':
+    unittest.main()
